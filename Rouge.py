36dd50180250fcb4922f0cc0ae9affaf9b033ea6
Yazid Bounab <bounab.yazid@gmail.com>
2019-01-03 16:58:09 +0200

    Update Rouge.py

--- Rouge.py
+++ Rouge.py
@@ -5,51 +5,58 @@ Created on Sun Dec 30 16:00:19 2018
 
 @author: Yazid Bounab
 """
+import os
+from os import listdir
+from os.path import isfile, join
+
 import nltk
 from nltk.util import ngrams
 from nltk.tokenize import word_tokenize
 from nltk.translate.bleu_score import sentence_bleu
 from Summarization import Summarize_Story
 
+#_______________________________________________________________
 
 #https://rxnlp.com/how-rouge-works-for-evaluation-of-summarization-tasks/#.XCXw9MaxU5k
 #https://machinelearningmastery.com/calculate-bleu-score-for-text-python/
 
-#_______________________________________________________________
-
 def Rouge_N(N,MSummary, RefSummary):
-        
     tokens1 = word_tokenize(MSummary)
     Ngrams1 = list(ngrams(tokens1,N))
 
     tokens2 = word_tokenize(RefSummary)
     Ngrams2 = list(ngrams(tokens2,N))
     
+    Bleu = 0
+    Recall = 0
+    Precision = 0
+    F = 0
+      
     OverlapNgram = len([Ngram for Ngram in Ngrams1 if Ngram in Ngrams2])
        
     Recall = OverlapNgram/len(Ngrams2)
     Precision = OverlapNgram/len(Ngrams1)
-    F = (1+pow(N,2))*(Precision*Recall/(pow(N,2)*Precision)+ Recall)
+    
+    if OverlapNgram != 0:
+       F = (1+pow(N,2))*(Precision*Recall/(pow(N,2)*Precision)+ Recall)
 
     weights = [0] * 4
     weights[N-1] = 1
     weights = tuple(weights)
-    print (weights)
+    #print (weights)
     Bleu = sentence_bleu([RefSummary.split()], MSummary.split(), weights)
-
+    #Bleu = sentence_bleu([RefSummary.split()], MSummary.split())
     
-    print ('Bleu'+str(N)+' = ',Bleu)
-    print ('R'+str(N)+'_Recall = ',Recall)
-    print ('R'+str(N)+'_Precision = ',Precision)
-    print ('F'+str(N)+' = ',F)
-        
+    #print ('Bleu'+str(N)+' = ',Bleu)
+    #print ('R'+str(N)+'_Recall = ',Recall)
+    #print ('R'+str(N)+'_Precision = ',Precision)
+    #print ('F'+str(N)+' = ',F)
+    
+    return Bleu,Recall,Precision,F
+
 #_______________________________________________________________
 
 def Rouge1(MSummary, RefSummary):
-        
-    #Text1 = 'the cat was found under the bed'
-    #Text2 = 'the cat was under the bed'
-    
     OverlapWords = len([w for w in MSummary.split() if w in RefSummary.split()])
        
     Recall = OverlapWords/len(RefSummary.split())
@@ -64,13 +71,13 @@ def Rouge1(MSummary, RefSummary):
     print ('R1_Recall = ',Recall)
     print ('R1_Precision = ',Precision)
     print ('F1 = ',F1)
+    
+    return Bleu,Recall,Precision,F1
+
 
 #_______________________________________________________________
     
 def Rouge2(MSummary, RefSummary):  
-    #Text1 = 'the cat was found under the bed'
-    #Text2 = 'the cat was under the bed'
-    
     tokens1 = word_tokenize(MSummary)
     bigrms1 = nltk.bigrams(tokens1)
     bigrms1 = list(ngrams(tokens1,2))
@@ -87,45 +94,126 @@ def Rouge2(MSummary, RefSummary):
     Precision = OverlapBigrams/len(bigrms1)
     
     Bleu = sentence_bleu([RefSummary.split()], MSummary.split(), weights=(0, 1, 0, 0))
-    F2 = (1+pow(1,2))*(Precision*Recall/(pow(1,2)*Precision)+ Recall)
+    F2 = (1+pow(2,2))*(Precision*Recall/(pow(2,2)*Precision)+ Recall)
 
     print ('Bleu2 = ',Bleu)
     print ('R2_Recall = ',Recall)
     print ('R2_Precision = ',Precision)
     print ('F2 = ',F2)
+    
+    return Bleu,Recall,Precision,F2
 
 #_______________________________________________________________
 
 def Summary_Evaluation(MSummary,RefSummary,OriginalDoc):
-    with open(MSummary, encoding="utf8") as f:
+    with open(MSummary, encoding="ISO-8859-1") as f:
          Text1 = f.read()
          f.close()
          
-    with open(RefSummary, encoding="utf8") as f:
+    with open(RefSummary, encoding="ISO-8859-1") as f:
          Text2 = f.read()
          f.close()
     
-    with open(OriginalDoc, encoding="utf8") as f:
+    with open(OriginalDoc, encoding="ISO-8859-1") as f:
          Text3 = f.read()
          f.close()
          
-    Rouge1(Text1, Text2)
-    print ('_____________________________')
-    Rouge2(Text1, Text2)
-    print ('_____________________________')
+    Bleu1 = 0
+    Recall1 = 0
+    Precision1 = 0
+    F1= 0
     
-    Total_words_summary = len(Text1.split())
-    Total_words_original = len(Text3.split())
-    Compressed_Rate = Total_words_summary / Total_words_original
+    Bleu2 = 0
+    Recall2 = 0
+    Precision2 = 0
+    F2 = 0
+    Compressed_Rate = 0
     
-    print ('Compressed Rate = ',Compressed_Rate)
+    if Text1: 
+       #Bleu1,Recall1,Precision1,F1 = Rouge1(Text1, Text2)
+       #print ('.............................')
+       Bleu1,Recall1,Precision1,F1 = Rouge_N(1,Text1, Text2)
+       #print ('_____________________________')
+    
+       #Bleu2,Recall2,Precision2,F2 = Rouge2(Text1, Text2)
+       #print ('.............................')
+       Bleu2,Recall2,Precision2,F2 = Rouge_N(2,Text1, Text2)
+       #print ('_____________________________')
+    
+       Total_words_summary = len(Text1.split())
+       Total_words_original = len(Text3.split())
+       Compressed_Rate = Total_words_summary / Total_words_original
+    
+        #print ('Compressed Rate = ',Compressed_Rate)
+    
+    return Bleu1,Recall1,Precision1,F1,Bleu2,Recall2,Precision2,F2,Compressed_Rate
 
 #_______________________________________________________________
+    
+def Evaluate_BBC_News_Summary():
+    News_Articles = '/home/polo/.config/spyder-py3/Co-referece/BBC News Summary/News Articles'
+    Summaries = '/home/polo/.config/spyder-py3/Co-referece/BBC News Summary/Summaries'
+    Machine_Summary = '/home/polo/.config/spyder-py3/Co-referece/BBC News Summary/Machine Summary'
+    Evaluation_Summary = '/home/polo/.config/spyder-py3/Co-referece/BBC News Summary/Evaluation Summary'
 
-News_Articles = '/home/polo/.config/spyder-py3/Co-referece/BBC News Summary/News Articles/business'
-Summaries = '/home/polo/.config/spyder-py3/Co-referece/BBC News Summary/Summaries/business'
-Machine_Summary = '/home/polo/.config/spyder-py3/Co-referece/BBC News Summary/Machine Summary/business'
- 
+    #Summary_Evaluation(Machine_Summary+'/001.txt', Summaries+'/001.txt',News_Articles+'/001.txt')
+    SubDirectories = os.listdir(News_Articles)
+
+    try:  
+        os.mkdir(Evaluation_Summary)
+    except OSError:  
+                print ("Creation of the directory %s failed" % Evaluation_Summary)
+                
+    for subdir in SubDirectories:
+        try:  
+            os.mkdir(Evaluation_Summary+'/'+subdir)
+        except OSError:  
+                print ("Creation of the directory %s failed" % subdir)
+    
+       
+    for subdir in SubDirectories:
+        FBleu1 = open(Evaluation_Summary+'/'+subdir+'/'+"Bleu1.txt","w+")
+        FRecall1 = open(Evaluation_Summary+'/'+subdir+'/'+"Recall1.txt","w+")
+        FPrecision1 = open(Evaluation_Summary+'/'+subdir+'/'+"Precision1.txt","w+")
+        FF1 = open(Evaluation_Summary+'/'+subdir+'/'+"F1.txt","w+")
+    
+        FBleu2 = open(Evaluation_Summary+'/'+subdir+'/'+"Bleu2.txt","w+")
+        FRecall2 = open(Evaluation_Summary+'/'+subdir+'/'+"Recall2.txt","w+")
+        FPrecision2= open(Evaluation_Summary+'/'+subdir+'/'+"Precision2.txt","w+")
+        FF2 = open(Evaluation_Summary+'/'+subdir+'/'+"F2.txt","w+")
+    
+        FComp_Rate = open(Evaluation_Summary+'/'+subdir+'/'+"Compressed_Rate.txt","w+")
+
+        files = [f for f in listdir(News_Articles+'/'+subdir) if isfile(join(News_Articles+'/'+subdir, f))]    
+
+        for f in files:
+            print(News_Articles+'/'+subdir+'/'+f)
+
+            Bleu1,Recall1,Precision1,F1,Bleu2,Recall2,Precision2,F2,Compressed_Rate = Summary_Evaluation(Machine_Summary+'/'+subdir+'/'+f, Summaries+'/'+subdir+'/'+f,News_Articles+'/'+subdir+'/'+f)
+            
+            FBleu1.write(str(Bleu1)+'\n')
+            FRecall1.write(str(Recall1)+'\n')
+            FPrecision1.write(str(Precision1)+'\n')
+            FF1.write(str(F1)+'\n')
+            
+            FBleu2.write(str(Bleu2)+'\n')
+            FRecall2.write(str(Recall2)+'\n')
+            FPrecision2.write(str(Precision2)+'\n')
+            FF2.write(str(F2)+'\n')
+            
+            FComp_Rate.write(str(Compressed_Rate)+'\n')
+            
+        FBleu1.close()
+        FRecall1.close()
+        FPrecision1.close()
+        FF1.close()
+    
+        FBleu2.close()
+        FRecall2.close()
+        FPrecision2.close()
+        FF2.close()
+    
+        FComp_Rate.close()
 #_______________________________________________________________
 
-Summary_Evaluation(Machine_Summary+'/001.txt', Summaries+'/001.txt',News_Articles+'/001.txt')
+Evaluate_BBC_News_Summary()
